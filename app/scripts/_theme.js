4180adacf5ead73ff2f2dec69443fa2388be840c
Lera <kalinina.valeri@gmail.com>
2019-08-01 07:23:34 -0400

    menu add

--- app/scripts/_theme.js
+++ app/scripts/_theme.js
@@ -1,3 +1,9 @@
 // custom global script
+var btnMenu = document.getElementById('btn--menu');
+var modalMenu = document.getElementById('menu');
+
+btnMenu.addEventListener('click', function(){
+    modalMenu.classList.toggle('active');
+})
 
 // console.log('test');
