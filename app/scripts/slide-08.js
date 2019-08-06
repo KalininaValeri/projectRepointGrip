bb44fa60d290faaac7460f394bac5a92b1ccff4c
Lera <kalinina.valeri@gmail.com>
2019-08-06 02:20:37 -0400

    slide-08

--- app/scripts/slide-08.js
+++ app/scripts/slide-08.js
@@ -1,4 +1,13 @@
 /* global cegedim*/
 $(function() {
+  var content = document.getElementById('content');
+  var women1 = document.getElementById('women-1');
+  var women2 = document.getElementById('women-2');
 
+  content.addEventListener('click', function() {
+    if (women1.classList.contains('active')) {
+      women1.classList.remove('active');
+      women2.classList.add('active');
+    }
+  })
 });
