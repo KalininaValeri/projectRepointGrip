9f2f634216c4bd14ab0b64730bda245991b0f55d
Lera <kalinina.valeri@gmail.com>
2019-08-06 01:11:48 -0400

    slide-05

--- app/scripts/slide-05.js
+++ app/scripts/slide-05.js
@@ -1,4 +1,18 @@
 /* global cegedim*/
 $(function() {
+  var trigger = document.getElementById('trigger');
+  var content = document.getElementById('content');
 
+  console.log(trigger);
+
+  trigger.addEventListener('click', function() {
+    if (content.classList.contains('day')) {
+      content.classList.remove('day');
+      content.classList.add('night');
+      return false;
+    }
+
+    content.classList.remove('night');
+    content.classList.add('day');
+  })
 });
