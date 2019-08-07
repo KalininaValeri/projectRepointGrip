56c66e558d47a9535288dc319f0d6990acd10d53
Lera <kalinina.valeri@gmail.com>
2019-08-07 01:32:51 -0400

    rushe4ka

--- app/scripts/_theme.js
+++ app/scripts/_theme.js
@@ -2,8 +2,52 @@
 var btnMenu = document.getElementById('btn--menu');
 var modalMenu = document.getElementById('menu');
 
+
 btnMenu.addEventListener('click', function(){
     modalMenu.classList.toggle('active');
 })
+
+var removeLogoHeaderClassActive = function() {
+  var arrImgHeader = document.getElementsByClassName('header__logo');
+  for (var i = 0; i<arrImgHeader.length; i++) {
+    arrImgHeader[i].classList.remove('active')
+  }
+}
+
+if (
+  cegedim.slide.ID === '01_1' ||
+  cegedim.slide.ID === '02_1' ||
+  cegedim.slide.ID === '03_1' ||
+  cegedim.slide.ID === '04_1' ||
+  cegedim.slide.ID === '05_1' ||
+  cegedim.slide.ID === '06_1'
+) {
+  removeLogoHeaderClassActive()
+  document.getElementsByClassName('header__logo--kaps')[0].classList.add('active');
+}
+
+if (
+  cegedim.slide.ID === '07_2' ||
+  cegedim.slide.ID === '08_2' ||
+  cegedim.slide.ID === '09_2' ||
+  cegedim.slide.ID === '10_2' ||
+  cegedim.slide.ID === '11_2' ||
+  cegedim.slide.ID === '12_2' ||
+  cegedim.slide.ID === '13_2'
+) {
+  removeLogoHeaderClassActive()
+  document.getElementsByClassName('header__logo--steak')[0].classList.add('active');
+}
+
+if (
+  cegedim.slide.ID === '13_3' ||
+  cegedim.slide.ID === '14_3' ||
+  cegedim.slide.ID === '15_3' ||
+  cegedim.slide.ID === '16_3' ||
+  cegedim.slide.ID === '17_3'
+) {
+  removeLogoHeaderClassActive()
+  document.getElementsByClassName('header__logo--drink')[0].classList.add('active');
+}
 
 // console.log('test');
